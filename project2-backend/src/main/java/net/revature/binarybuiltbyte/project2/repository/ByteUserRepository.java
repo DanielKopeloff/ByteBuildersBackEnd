3902393df04398311ca09c606bb54b0916b9521e
Jl3E <joey.elmblad@gmail.com>
2021-04-15 16:37:12 -0700

    added ProductRepository method for search bar, and for user shopping history

--- project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/repository/ByteUserRepository.java
+++ project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/repository/ByteUserRepository.java
@@ -1,9 +1,16 @@
 package net.revature.binarybuiltbyte.project2.repository;
 
 import net.revature.binarybuiltbyte.project2.model.ByteUser;
+import net.revature.binarybuiltbyte.project2.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.sql.Date;
+import java.util.List;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "byteUsers", path = "byte-user")
