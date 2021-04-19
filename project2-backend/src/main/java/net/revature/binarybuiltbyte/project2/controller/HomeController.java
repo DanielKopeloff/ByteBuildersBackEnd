a3f2d4ef67d8de418e1a211a7f6991809a0f3ec9
Daniel Reyes <35903470+DanRey321@users.noreply.github.com>
2021-04-19 07:55:45 -0700

    CrossOrigin

--- project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/controller/HomeController.java
+++ project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/controller/HomeController.java
@@ -10,11 +10,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 public class HomeController {
     @Autowired
