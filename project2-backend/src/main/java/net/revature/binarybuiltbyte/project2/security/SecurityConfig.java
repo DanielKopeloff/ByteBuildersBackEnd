219cf7635cac57150d0eda34ff35f0b78b0ef566
Jl3E <joey.elmblad@gmail.com>
2021-04-20 08:32:15 -0700

    added .antMatchers

--- project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/security/SecurityConfig.java
+++ project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/security/SecurityConfig.java
@@ -4,14 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -37,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/authenticate")
+                .antMatchers("/**","**/login/**","/authenticate", "/api/product/**", "/api/address" , "/api/category/**", "/api/byte-user/**", "/api/review**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
