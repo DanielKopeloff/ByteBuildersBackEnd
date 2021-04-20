3b8743577aff3f617954280ee5764fd3b328ec84
Jl3E <joey.elmblad@gmail.com>
2021-04-20 09:02:48 -0700

    added .antMatchers ADDED COMMENT ON LINE 34 IN SECURITYcONFIG

--- project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/security/SecurityConfig.java
+++ project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+        //something
         http.csrf()
                 .disable()
                 .authorizeRequests()
