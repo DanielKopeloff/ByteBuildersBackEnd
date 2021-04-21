a450e5e4dc98203c4609babe553713702b144c2e
Daniel Reyes <35903470+DanRey321@users.noreply.github.com>
2021-04-20 20:57:31 -0700

    testing

--- project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/security/JwtResponse.java
+++ project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/security/JwtResponse.java
@@ -1,18 +1,43 @@
 package net.revature.binarybuiltbyte.project2.security;
 
+import net.revature.binarybuiltbyte.project2.model.ByteUser;
+
+import java.util.Date;
+
 //@Data
 //@AllArgsConstructor
 //@NoArgsConstructor
 public class JwtResponse {
 
     private final String jwtToken;
+    private ByteUser user;
+    private Date date;
 
 
-    public JwtResponse(String jwtToken) {
+    public JwtResponse(String jwtToken, ByteUser user, Date date) {
         this.jwtToken = jwtToken;
+        this.user = user;
+        this.date = date;
     }
 
     public String getJwtToken(){
         return jwtToken;
     }
+
+    public int getUserId(){
+        return user.getId();
+    }
+    public String getUserEmail(){
+        return user.getEmail();
+    }
+    public String getName(){
+        return user.getFirstName();
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
 }
+
+// Wonder whast gonna happen
