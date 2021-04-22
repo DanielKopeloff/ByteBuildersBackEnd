3fe3d7c33a1f9a9c0fe466506418fb1d9bb405e5
DanielKopeloff <79463544+DanielKopeloff@users.noreply.github.com>
2021-04-22 11:57:58 -0400

    Revert "Ronald lopez 2"

--- project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/model/ByteOrder.java
+++ project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/model/ByteOrder.java
@@ -1,8 +1,10 @@
 package net.revature.binarybuiltbyte.project2.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -12,60 +14,38 @@ import java.util.Set;
 
 @Entity
 @Table(name = "byte_order")
-@Getter
-@Setter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ByteOrder {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private int totalQuantity;
-
-    private double totalPrice;
-
     @Enumerated(EnumType.ORDINAL)
     private Status status;
+
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private ByteUser byteUser;
 
     @Column(name = "order_created")
     @CreationTimestamp
     private Date orderCreated;
 
     @Column(name = "order_completed")
-    @UpdateTimestamp
     private Date orderCompleted;
 
     @OneToMany(mappedBy = "byteOrder")
     private Set<ProductOrder> productOrders = new HashSet<>();
-
-    @ManyToOne(
-            fetch = FetchType.LAZY,
-            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinColumn(name = "byte_user_id")
-    private ByteUser byteUser;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "shipping_address_id", referencedColumnName = "id")
-    private Address shippingAddress;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "billing_address_id", referencedColumnName = "id")
-    private Address billingAddress;
 
     @OneToMany(
             fetch = FetchType.LAZY,
             cascade = CascadeType.ALL,
             mappedBy="byteOrder")
     private List<Review> reviews;
-
-    public void add(ProductOrder productOrder) {
-        if (productOrder != null) {
-            if (productOrders == null) {
-                productOrders = new HashSet<>();
-            }
-            productOrders.add(productOrder);
-            productOrder.setByteOrder(this);
-        }
-    }
 
 }
